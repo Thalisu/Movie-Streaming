0a264510b706121f9df999d882d970b48cfbc28a
Thalisu <thalisondmcc@gmail.com>
2024-10-15 12:06:27 -0300

    refactor: change loading keyframes to globalStyles.css file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./styles/globalStyles.css";
 import { GlobalStyle } from "./styles/globalStyles.ts";
-
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
